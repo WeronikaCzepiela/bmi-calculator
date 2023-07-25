d4994096ff598d4c5ee64ec5c72e78e8f2828a51
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-07-25 21:52:33 +0200

    Start changing layout for desktop version

--- src/components/Middle/Middle.jsx
+++ src/components/Middle/Middle.jsx
@@ -1,12 +1,17 @@
 import './Middle.scss'
 import { Chart } from '../Chart/Chart'
 import { Data } from '../DataAndResult/Data/Data'
+import { useWindowDimensions } from '../../utils/hookers/useWindowDimensions'
+import { dimension } from '../../utils/Dimension'
 
 export const Middle = ({ setNewMeasurementsHistory, measurementsHistory }) => {
+  const { width } = useWindowDimensions()
   return (
-    <div className={'middle'}>
-      <Data setNewMeasurementsHistory={setNewMeasurementsHistory} />
-      <Chart measurementsHistory={measurementsHistory} />
+    <div className={`middle ${width > dimension() && 'middle-desktop'}`}>
+      <div className={`border-line ${width > dimension() && 'border-line-desktop'}`}>
+        <Data setNewMeasurementsHistory={setNewMeasurementsHistory} />
+        <Chart measurementsHistory={measurementsHistory} />
+      </div>
     </div>
   )
 }
