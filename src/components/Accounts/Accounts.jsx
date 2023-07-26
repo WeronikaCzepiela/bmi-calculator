8dc310a5df5f2124981c545a0369bc8ff7e45498
WeronikaCzepiela <weronika.czepiela@gmail.com>
2023-07-26 08:25:25 +0200

    Add layout for desktop version for accounts section

--- src/components/Accounts/Accounts.jsx
+++ src/components/Accounts/Accounts.jsx
@@ -1,11 +1,17 @@
 import './Accounts.scss'
 import LinkedIn from './images/LinkedIn.png'
 import GitHub from './images/GitHub.png'
+import { useWindowDimensions } from '../../utils/hookers/useWindowDimensions'
+import { dimension } from '../../utils/Dimension'
 
 export const Accounts = () => {
+  const { width } = useWindowDimensions()
+
   return (
-    <div className={'accounts'}>
-      <p className={'accounts-text'}>Check my accounts</p>
+    <div className={`accounts ${width > dimension() && 'accounts-desktop'}`}>
+      <p className={`accounts-text ${width > dimension() && 'accounts-text-desktop'}`}>
+        Check my accounts
+      </p>
       <a target={'_blank'} href={'https://github.com/WeronikaCzepiela'}>
         <img src={GitHub} />
       </a>
