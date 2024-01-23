6a42b8751e19d562f0373766dd78654b8095487d
WeronikaCzepiela <weronika.czepiela@gmail.com>
2024-01-23 17:50:40 +0100

    refactor text

--- src/components/DataAndResult/Data/RadioGroup/RadioGroup.tsx
+++ src/components/DataAndResult/Data/RadioGroup/RadioGroup.tsx
@@ -2,16 +2,20 @@ import './RadioGroup.scss'
 import { Text } from '../../../Text/Text'
 import { useWindowDimensions } from '../../../../utils/hookers/useWindowDimensions'
 import { dimension } from '../../../../utils/Dimension'
+import { RadioGroupTypes } from './RadioGroup.types'
+import { TypesOfText } from '../../../Text/Text.types'
+import { OptionsTypes } from '../TextInput/Options.types'
 
-export const RadioGroup = ({ options, name, label, onChange, defaultValue }) => {
+export const RadioGroup = ({ options, name, label, onChange, defaultValue }: RadioGroupTypes) => {
   const { width } = useWindowDimensions()
+
   const handleChange = (selectedOption) => {
     onChange(selectedOption.target.value)
   }
 
   return (
     <div className={`radio-group ${width > dimension() && 'radio-group-desktop'}`}>
-      <Text type={'l'}>{label}</Text>
+      <Text type={TypesOfText.L}>{label}</Text>
       {options.map(({ label, value }) => {
         return (
           <div key={value} className={'radio-option'} onChange={handleChange}>
